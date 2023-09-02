83d3d8a2ab611d912f5c38bc2c950dff0625c909
Akatsuzi <42118269+Suzie1@users.noreply.github.com>
2023-09-02 11:35:18 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,43 @@
+#from .CR_Animation_Nodes import *
+from .nodes.interpolation import *
+#from .nodes.io import *
+from .nodes.prompt import *
+#from .nodes.schedulers import *
+from .nodes.lists import *
+from .nodes.utils import *
+#from .nodes.camera import *
+from .nodes.cyclers import *
+
+NODE_CLASS_MAPPINGS = {
+    ### Drop 1 - 12 nodes
+    # Prompt Keyframes
+    "CR Prompt List":CR_PromptList,
+    "CR Prompt List Keyframes":CR_PromptListKeyframes,
+    "CR Animation Stack":CR_AnimationStack,    
+    "CR Animation Stack Keyframes":CR_AnimationStackKeyframes,
+    "CR Keyframe List":CR_KeyframeList,    
+    "CR Prompt Text":CR_PromptText,    
+    # Interpolate
+    "CR Gradient Float":CR_GradientFloat,
+    "CR Gradient Integer":CR_GradientInteger,
+    "CR Increment Float":CR_IncrementFloat,    
+    "CR Increment Integer":CR_IncrementInteger,   
+    ### Lists
+    "CR Model List":CR_ModelList,
+    "CR LoRA List":CR_LoRAList,
+    ### Cyclers
+    "CR Cycle Models":CR_CycleModels,    
+    "CR Cycle LoRAs":CR_CycleLoRAs,
+    # Index
+    "CR Index Increment":CR_IncrementIndex,
+    "CR Index Multiply":CR_MultiplyIndex,
+    "CR Index Reset":CR_IndexReset,      
+    # Utils
+    "CR Debatch Frames":CR_DebatchFrames,    
+    "CR Text List To String":CR_TextListToString,    
+    "CR Current Frame":CR_CurrentFrame,
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS']
+
+print("\033[34mCR Animation Nodes: \033[92mLoaded\033[0m")
